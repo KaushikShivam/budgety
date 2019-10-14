9b7a7e229f3e037d3c5224430c0c33630e8ec301
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 15:02:33 +0530

    Refactor dom query strings

--- app.js
+++ app.js
@@ -3,18 +3,30 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	};
+
 	return {
 		getInput: function() {
 			return {
-				type: document.querySelector('.add__type').value,
-				description: document.querySelector('.add__description').value,
-				value: document.querySelector('.add__value').value
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value
 			};
+		},
+		getDOMstrings: function() {
+			return DOMstrings;
 		}
 	};
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
+	var DOM = UICtrl.getDOMstrings();
+
 	var ctrlAddItem = function() {
 		// 1. Get field input data
 		var input = UICtrl.getInput();
@@ -26,7 +38,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 5. Display budget on the UI
 	};
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function(event) {
 		if (event.keyCode === 13 || event.which === 13) {
