e16c275f2e204b690df753d1e49433e849a6fe48
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 15:10:26 +0530

    Add initialisation function

--- app.js
+++ app.js
@@ -25,7 +25,17 @@ var UIController = (function() {
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
 
 	var ctrlAddItem = function() {
 		// 1. Get field input data
@@ -38,11 +48,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 5. Display budget on the UI
 	};
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			// console.log('app started');
+			setupEventListeners();
 		}
-	});
+	};
 })(budgetController, UIController);
+
+controller.init();
