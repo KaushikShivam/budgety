76dfbc64cd90a542183002fe1f7913c0fc3d833f
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 14:08:42 +0530

    Implement module pattern

---
+++ app.js
@@ -0,0 +1,17 @@
+var budgetController = (function() {
+	var x = 23;
+
+	var add = function(a) {
+		return x + a;
+	};
+
+	return {
+		publicTest: function(b) {
+			return add(b);
+		}
+	};
+})();
+
+var UIController = (function() {})();
+
+var controller = (function(budgetCtrl, UICtrl) {})(budgetController, UIController);
