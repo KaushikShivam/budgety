0340690b8fb506a6885af905662448d2a5aea47c
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 14:58:01 +0530

    Add input read method in UIctrl

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-	/// Some code
+	return {
+		getInput: function() {
+			return {
+				type: document.querySelector('.add__type').value,
+				description: document.querySelector('.add__description').value,
+				value: document.querySelector('.add__value').value
+			};
+		}
+	};
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
 	var ctrlAddItem = function() {
 		// 1. Get field input data
+		var input = UICtrl.getInput();
+		console.log(input);
+
 		// 2. Get item to budget controller
 		// 3. Add item to user interface
 		// 4. Calculate the budget
