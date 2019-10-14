ecc17e2496aea6507817c40d611d2e42c69835b3
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 17:46:56 +0530

    Update UI for the budget controller

--- app.js
+++ app.js
@@ -89,7 +89,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	};
 
 	return {
@@ -137,6 +141,18 @@ var UIController = (function() {
 			fieldsArr[0].focus();
 		},
 
+		displayBudget: function(obj) {
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -159,7 +175,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 	var updateBudget = function() {
 		budgetCtrl.calculateBudget();
 		var budget = budgetCtrl.getBudget();
-		console.log(budget);
+		UICtrl.displayBudget(budget);
 	};
 
 	var ctrlAddItem = function() {
