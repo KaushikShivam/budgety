1474ad1608c2fb48de515f7087c19ccd23f73ebb
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 16:45:34 +0530

    Add new item to the UI

--- app.js
+++ app.js
@@ -52,7 +52,9 @@ var UIController = (function() {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputBtn: '.add__btn'
+		inputBtn: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	};
 
 	return {
@@ -63,6 +65,29 @@ var UIController = (function() {
 				value: document.querySelector(DOMstrings.inputValue).value
 			};
 		},
+
+		addListItem: function(obj, type) {
+			// Create html string with placeholder text
+			var html, newHtml, element;
+			if (type === 'inc') {
+				element = DOMstrings.incomeContainer;
+				html =
+					'<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else {
+				element = DOMstrings.expensesContainer;
+				html =
+					'<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			//Replace placeholder text with data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+
+			// Insert html into the data
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -89,6 +114,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		// 3. Add item to user interface
+		UICtrl.addListItem(newItem, input.type);
 		// 4. Calculate the budget
 		// 5. Display budget on the UI
 	};
