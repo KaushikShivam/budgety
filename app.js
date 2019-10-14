ed75b34447d8870ed118463d1d8cf5e1a172e8ea
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-14 14:21:07 +0530

    Setup event listeners for budgety

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var budgetController = (function() {
-	var x = 23;
-
-	var add = function(a) {
-		return x + a;
-	};
-
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	};
+	//Some code
 })();
 
-var UIController = (function() {})();
+var UIController = (function() {
+	/// Some code
+})();
 
-var controller = (function(budgetCtrl, UICtrl) {})(budgetController, UIController);
+var controller = (function(budgetCtrl, UICtrl) {
+	var ctrlAddItem = function() {
+		// 1. Get field input data
+		// 2. Get item to budget controller
+		// 3. Add item to user interface
+		// 4. Calculate the budget
+		// 5. Display budget on the UI
+	};
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
+})(budgetController, UIController);
